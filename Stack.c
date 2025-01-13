6d625e98c84f22616ded9ff289457481fa751942
Kavyan Nayak <kavyannayak104@gmail.com>
2025-01-13 12:29:40 +0530

    Satck

--- Stack.c
+++ Stack.c
@@ -1,28 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>  // For exit()
 #define SIZE 5
 
 int stack[SIZE];
 int top = -1;  // Empty stack
 
-
-//  Push function
+// Push function
 void push(int num)
 {
-    // check if stack is full
-    if(top == SIZE-1)
+    // Check if stack is full
+    if (top == SIZE - 1)
     {
         printf("Stack is full\n");
     }
-    else{   // Increment top and add element
+    else
+    { 
         top++;
         stack[top] = num;
+        printf("%d pushed to the stack\n", num);
     }
 }
 
 int empty()
 {
-    // when stack is empty
-    if(top == -1)
+    // Check if stack is empty
+    if (top == -1)
     {
         return 1;
     }
@@ -32,26 +34,34 @@ int empty()
     }
 }
 
-
 int pop()
 {
-    // check if stack is empty
-    if(empty())
+    // Check if stack is empty
+    if (empty())
     {
-        printf("Stack UNDEFLOW\n");
+        printf("Stack UNDERFLOW\n");
         return -1;
-    }else{
+    }
+    else
+    {
         return stack[top--];
     }
 }
 
-
 void display()
 {
     int i;
-    for (i = top; i >= 0; i--)
+    if (empty())
     {
-        printf("%d\n", stack[i]);   
+        printf("Stack is empty\n");
+    }
+    else
+    {
+        printf("Stack elements are:\n");
+        for (i = top; i >= 0; i--)
+        {
+            printf("%d\n", stack[i]);
+        }
     }
 }
 
@@ -59,28 +69,34 @@ int peek()
 {
     if (empty())
     {
+        printf("Stack is empty\n");
         return -1;
     }
-    else{
+    else
+    {
         return stack[top];
-    }   
+    }
 }
 
 int peep(int location)
 {
     int index;
-    if(top == -1)
+    if (empty())
     {
+        printf("Stack is empty\n");
         return -1;
     }
-    else{
-        index = top - location + 1;  // 0
-        if(index < 0 || index > SIZE-1)
+    else
+    {
+        index = top - location + 1;  
+        if (index < 0 || index > top)
         {
+            printf("Invalid location\n");
             return -1;
         }
-        else{
-            return stack[top - location + 1];
+        else
+        {
+            return stack[index];
         }
     }
 }
@@ -89,62 +105,64 @@ int main()
 {
     int choice, num, location;
 
-
-    printf("1. Push\n");
-    printf("2. Pop\n");
-    printf("3. Display\n");
-    printf("4. Peek\n");
-    printf("5. Peep\n");
-    printf("6. Exit\n");
-
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-
-    switch (choice)
+    while (1)
     {
-    case 1:
-        printf("Enter the number to push: ");
-        scanf("%d", &num);
-        push(num);
-        break;
+        printf("\n1. Push\n");
+        printf("2. Pop\n");
+        printf("3. Display\n");
+        printf("4. Peek\n");
+        printf("5. Peep\n");
+        printf("6. Exit\n");
 
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    case 2:
-        num = pop();  // -1
-        if (num != -1)
+        switch (choice)
         {
-            printf("%d Poped",num);
-        }        
-        break;
+        case 1:
+            printf("Enter the number to push: ");
+            scanf("%d", &num);
+            push(num);
+            break;
 
-    case 3:
-        display();
-        break;    
+        case 2:
+            num = pop();
+            if (num != -1)
+            {
+                printf("%d popped from the stack\n", num);
+            }
+            break;
 
-    case 4:
-        printf("Enter the location to peek: ");
-        scanf("%d", &location);
-        num = peep(location);
+        case 3:
+            display();
+            break;
 
-        if (num != -1)
-        {
-            printf("The number at %d is %d", location, num);
+        case 4:
+            num = peek();
+            if (num != -1)
+            {
+                printf("The number at the top is %d\n", num);
+            }
+            break;
+
+        case 5:
+            printf("Enter the location to peep: ");
+            scanf("%d", &location);
+            num = peep(location);
+            if (num != -1)
+            {
+                printf("The number at location %d is %d\n", location, num);
+            }
+            break;
+
+        case 6:
+            printf("Exiting program...\n");
+            exit(0);
+
+        default:
+            printf("Invalid choice\n");
+            break;
         }
-        break;
-
-    case 5:
-        num = peek();
-        if (num != -1)
-        {
-            printf("The number at the top is %d", num);
-        }
-        break;
-
-    case 0:
-        exit(0);
-    default:
-        printf("Invalid choice\n");
-        break;
     }
     return 0;
 }
